da1d954c69330373f9b339f3d669d8663fcfc8e4
Горин Александр <pu1s@outlook.com>
2018-05-28 15:16:37 +0400

    add new cmake

---
+++ asterix/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdlib>
+#include "../libs/libasterix/src/libasterix.h"
+#pragma comment(lib, "libasterix.lib") 
+
+using namespace std;
+using namespace asterix::sys::types;
+//using namespace asterix::sys::util;
+int main()
+{
+	double rad = TO_RAD(56.5);
+	double grad = TO_GRAD(0.9);
+	asx_sexigesimal_arcdata_t t;
+	sexagesimal_tag* ff = new sexagesimal_tag();
+	ff->SetRad(0.9987);
+	ff->SetGrad(98.6);
+	//double aaa1 = ff->GradValue();
+	//asx_sexagesimal_util::asx_util_set_sexadesimal(10.00, &ff);
+	//asx_sexagesimal_format_tag sgft;
+	//wcscpy(sgft.deg_hrs, L"020111222");
+	//cout << sizeof(asx_sexagesimal_format_tag);
+	cout << "Hello CMake." << endl;
+#ifdef _WIN32
+	system("pause");
+#endif
+	return 0;
+}
