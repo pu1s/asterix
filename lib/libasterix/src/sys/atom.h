cb17e86d05d960074e3215df60ab5d994cb6a532
Горин Александр <pu1s@outlook.com>
2018-06-15 06:31:46 +0400

    ++++++++++++++++++++++++++++++++

--- lib/libasterix/src/sys/atom.h
+++ lib/libasterix/src/sys/atom.h
@@ -33,7 +33,7 @@ SOFTWARE.
 #include <string>
 #include <vector>
 
-#include "diagnostic.h"
+//#include "diagnostic.h"
 
 
 
@@ -45,230 +45,36 @@ namespace asx
 		namespace types
 		{
 
-			struct basic_point_prototype
+			struct iPrintable
 			{
 			public:
-				virtual std::string to_str() noexcept = 0;
-				//virtual std::string to_str(const std::ios::fmtflags& fl) noexcept = 0;
+				virtual std::string ToString() = 0;
 			};
 
-			template<typename _Value_Type, typename _Key_Type>
-			struct basic_dynamic_pair
+			
+			template<typename T>
+			struct point_atom : public iPrintable
 			{
-			protected:
-				_Key_Type *		_key;
-				_Value_Type*	_value;
-			public:
-
-				basic_dynamic_pair() noexcept
-				{
-					_key = new _Key_Type();
-					_value = new _Value_Type();
-
-				}
-					
-
-				basic_dynamic_pair(const _Value_Type& value, const _Key_Type& key) noexcept
-				{
-					_key = new _Key_Type(key);
-					_value = new _Value_Type(value);
-					
-				}
-				
-				~basic_dynamic_pair()
-				{
-#ifdef _DEBUG
-					std::cout << "basic_dynamic_pair " << "is deleted!" << std::endl;
-#endif // _DEBUG
-					delete _key;
-					delete _value;
-				}
-				
-				
-				/*void set(const _Value_Type& value, const _Key_Type& key) noexcept;
-				
-				basic_dynamic_pair& get() noexcept;
-				
-				_Key_Type& get_key() noexcept;
-				
-				_Value_Type& get_value() noexcept;
-
-
-				basic_dynamic_pair& operator =(const basic_dynamic_pair& other) noexcept;
-
-				bool operator ==(const basic_dynamic_pair& left) const noexcept;
-
-				bool operator !=(const basic_dynamic_pair& left) const noexcept;
-*/
-			};
-
-			template<typename T, typename U, template<class, class> class pair = basic_dynamic_pair>
-			struct dynamic_pair
-			{
-				typedef pair<T, U> dynamic_pair_t;
-				std::pair<int, int> p;
 			private:
-				dynamic_pair_t* _pair_data;
+				T* m_atom;
 			public:
-				dynamic_pair() noexcept
+				// Унаследовано через iPrintable
+				virtual std::string ToString() override
 				{
-					_pair_data = new dynamic_pair_t();
-#ifdef _DEBUG
-					std::cout << "dynamic_pair " << "is work!" << std::endl;
-#endif // _DEBUG
+					return std::to_string(*m_atom);
 				}
 
-				dynamic_pair(const T & val, const U& key) noexcept
-				{
-					_pair_data = new dynamic_pair_t(val, key);
-				}
-				~dynamic_pair()
-				{
-					//DEBUG_MESSAGE("aaa", asx::sys::diagnostic::asx_diagnostic_function_type::dtor);
-#ifdef _DEBUG
-					std::cout << "dynamic_pair " << "is deleted!" << std::endl;
-#endif // _DEBUG
-				}
 			};
 
-			struct basic_dynamic_point_prototype
+			template<typename T>
+			struct point
 			{
-				virtual std::string to_str() noexcept = 0;
-			};
-
-			/*
-			Basic representation on 2D point <STL variant>
-			*/
-			template<typename _Ty>
-			struct basic_point2 : public basic_point_prototype
-			{
-				_Ty X; // X
-				_Ty Y; // Y
-
-				/*
-				Ctor
-				*/
-				basic_point2()  noexcept
-				{
-					X = _Ty();
-					Y = _Ty();
-				}
-
-				/*
-				Ctor with params
-				*/
-				basic_point2(const _Ty& x, const _Ty& y) noexcept
-				{
-					X = x;
-					Y = y;
-				}
-
-				/*
-				To string
-				*/
-				std::string to_str() noexcept override
-				{
-					std::string x_str = std::to_string(X);
-					std::string y_str = std::to_string(Y);
-					std::string s("");
-					s.append("X= ");
-					s.append(x_str.c_str());
-					s.append(" ");
-					s.append("Y= ");
-					s.append(y_str.c_str());
-					s.append("\r\n");
-					return s;
-					
-				}
-			};
-
-			/*
-			Representation on 2D point with float value
-			*/
-			typedef  basic_point2<float> point2f;
-
-			/*
-			Representation on 2D point with double value
-			*/
-			typedef  basic_point2<double> point2d;
-
-			/*
-			Basic representation on 3D point <STL variant>
-			*/
-			template<typename _Ty>
-			struct basic_point3 : public basic_point_prototype
-			{
-				_Ty X;
-				_Ty Y;
-				_Ty Z;
-
-				/*
-				Ctor
-				*/
-				basic_point3() noexcept
-				{
-					_Ty X = _Ty();
-					_Ty Y = _Ty();
-					_Ty Z = _Ty();
-				}
-
-				/*
-				Ctor with params
-				*/
-				basic_point3(const _Ty& x, const _Ty& y, const _Ty z) noexcept
-				{
-					_Ty X = x;
-					_Ty Y = y;
-					_Ty Z = z;
-				}
-
-				/*
-				To string
-				*/
-				std::string to_str() noexcept override
-				{
-					std::string x_str = std::to_string(X);
-					std::string y_str = std::to_string(Y);
-					std::string z_str = std::to_string(Z);
-					std::string s("");
-					s.append("X= ");
-					s.append(x_str.c_str());
-					s.append(" ");
-					s.append("Y= ");
-					s.append(y_str.c_str());
-					s.append("Z= ");
-					s.append(z_str.c_str());
-					s.append("\r\n");
-					return s;
-				}
+				point_atom<T> X;
 			};
 		}
 	}
 }
 
 
-
 #include "atom.template"
-
-
-
-
-
-
-template<typename T, typename U>
-struct A
-{
-	T x;
-	U z;
-};
-
-template<typename T, typename U, template<class, class> class C = A>
-struct B
-{
-	C<T, U> y;
-};
-
-
- 
-
  
